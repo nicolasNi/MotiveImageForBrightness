5f928feec00210088c06011f9f183f6506a1a34a
Nick <nikaihua008@126.com>
2017-03-23 11:20:33 +0800

    修复了调节亮度时画面闪烁的bug

--- app/src/main/java/com/lt/nexthud/testservice/FxService.java
+++ app/src/main/java/com/lt/nexthud/testservice/FxService.java
@@ -1,6 +1,7 @@
 package com.lt.nexthud.testservice;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.os.Binder;
@@ -17,19 +18,10 @@ public class FxService extends Service {
     WindowManager.LayoutParams wmParams;
     //创建浮动窗口设置布局参数的对象
     WindowManager mWindowManager;
-
     private static final String TAG = "FxService";
-
-    private boolean serviceRunning = false;
-
     public ServiceBinder mBinder = new ServiceBinder(); /* 数据通信的桥梁 */
-
-    ImageView light1,light2,light3,light4,light5,light6,light7,light8,light9,light10,light11,light12,light13;
-
-    private int defaultLightNumber=3;
-
-    public FxService() {
-    }
+    ImageView light1, light2, light3, light4, light5, light6, light7, light8, light9, light10, light11, light12, light13;
+    private int defaultLightNumber = 3;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -40,37 +32,7 @@ public class FxService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        System.out.println("--onCreate()--");
-        serviceRunning = true;
         createFloatView();
-
-        new Thread() {
-            @Override
-            public void run() {
-                while (serviceRunning) {
-                    System.out.println("---Service运行中---");
-                    try {
-                        sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-        }.start();
-    }
-
-    // 每次启动Servcie时都会调用该方法
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        System.out.println("--onStartCommand()--");
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    // 解绑Servcie调用该方法
-    @Override
-    public boolean onUnbind(Intent intent) {
-        System.out.println("--onUnbind()--");
-        return super.onUnbind(intent);
     }
 
     // 退出或者销毁时调用该方法
@@ -78,137 +40,189 @@ public class FxService extends Service {
     public void onDestroy() {
         // TODO Auto-generated method stub
         super.onDestroy();
-        if(mFloatLayout != null)
-        {
+        if (mFloatLayout != null) {
             mWindowManager.removeView(mFloatLayout);
         }
     }
 
-
-    private void createFloatView()
-    {
+    private void createFloatView() {
         wmParams = new WindowManager.LayoutParams();
         //获取WindowManagerImpl.CompatModeWrapper
-        mWindowManager = (WindowManager)getApplication().getSystemService(getApplication().WINDOW_SERVICE);
+        mWindowManager = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
         //设置window type
         wmParams.type = android.view.WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
         //设置图片格式，效果为背景透明
         wmParams.format = PixelFormat.TRANSLUCENT;
+        //整个窗口的半透明值，1.0表示不透明，0.0表示全透明。
         wmParams.alpha = 0.9f;
-        wmParams.dimAmount = 0.9f;
+        //当FLAG_DIM_BEHIND设置后生效。该变量指示后面的窗口变暗的程度。
+        //1.0表示完全不透明，0.0表示没有变暗。
+        wmParams.dimAmount = 0.5f;
         //设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
         wmParams.flags =
                 android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                android.view.WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-//          LayoutParams.FLAG_NOT_TOUCHABLE
-        ;
-
+                        android.view.WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                        android.view.WindowManager.LayoutParams.FLAG_DIM_BEHIND;
         //调整悬浮窗显示的停靠位置为左侧置顶
         wmParams.gravity = Gravity.CENTER;
-
         // 以屏幕左上角为原点，设置x、y初始值
         wmParams.x = 0;
         wmParams.y = 0;
 
         // 设置悬浮窗口长宽数据
         wmParams.width = 300;
-        wmParams.height =300;
+        wmParams.height = 300;
 
-        //设置悬浮窗口长宽数据
-//        wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
-//        wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+//        LayoutInflater inflater = LayoutInflater.from(getApplication());
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        LayoutInflater inflater = LayoutInflater.from(getApplication());
         //获取浮动窗口视图所在布局
         mFloatLayout = (LinearLayout) inflater.inflate(R.layout.float_layout, null);
 
-        light1 = (ImageView)mFloatLayout.findViewById(R.id.light1);
-        light2 = (ImageView)mFloatLayout.findViewById(R.id.light2);
-        light3 = (ImageView)mFloatLayout.findViewById(R.id.light3);
-        light4 = (ImageView)mFloatLayout.findViewById(R.id.light4);
-        light5 = (ImageView)mFloatLayout.findViewById(R.id.light5);
-        light6 = (ImageView)mFloatLayout.findViewById(R.id.light6);
-        light7 = (ImageView)mFloatLayout.findViewById(R.id.light7);
-        light8 = (ImageView)mFloatLayout.findViewById(R.id.light8);
-        light9 = (ImageView)mFloatLayout.findViewById(R.id.light9);
-        light10 = (ImageView)mFloatLayout.findViewById(R.id.light10);
-        light11 = (ImageView)mFloatLayout.findViewById(R.id.light11);
-        light12 = (ImageView)mFloatLayout.findViewById(R.id.light12);
-        light13 = (ImageView)mFloatLayout.findViewById(R.id.light13);
+        light1 = (ImageView) mFloatLayout.findViewById(R.id.light1);
+        light2 = (ImageView) mFloatLayout.findViewById(R.id.light2);
+        light3 = (ImageView) mFloatLayout.findViewById(R.id.light3);
+        light4 = (ImageView) mFloatLayout.findViewById(R.id.light4);
+        light5 = (ImageView) mFloatLayout.findViewById(R.id.light5);
+        light6 = (ImageView) mFloatLayout.findViewById(R.id.light6);
+        light7 = (ImageView) mFloatLayout.findViewById(R.id.light7);
+        light8 = (ImageView) mFloatLayout.findViewById(R.id.light8);
+        light9 = (ImageView) mFloatLayout.findViewById(R.id.light9);
+        light10 = (ImageView) mFloatLayout.findViewById(R.id.light10);
+        light11 = (ImageView) mFloatLayout.findViewById(R.id.light11);
+        light12 = (ImageView) mFloatLayout.findViewById(R.id.light12);
+        light13 = (ImageView) mFloatLayout.findViewById(R.id.light13);
 
-        SetLight(defaultLightNumber);
+        setLight(defaultLightNumber);
         //添加mFloatLayout
-        try
-        {
+        try {
             mWindowManager.addView(mFloatLayout, wmParams);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
     public class ServiceBinder extends Binder {
-
-        /* 返回当前类的一个函数 */
-        public FxService getService() {
-            return FxService.this;
-        }
-
-        public void startDownload(int i) throws InterruptedException {
-            SetLight(defaultLightNumber+i);
+        public void changeLight(int i) {
+            setLight(defaultLightNumber + i);
         }
     }
 
-    private void SetLight(int lightNumber)
-    {
-        switch (lightNumber)
-        {
+    private void setLight(int lightNumber) {
+        switch (lightNumber) {
             case 0:
-                SwitchToDark(light1);SwitchToDark(light2);SwitchToDark(light3);SwitchToDark(light4);
-                SwitchToDark(light5);SwitchToDark(light6);SwitchToDark(light7);SwitchToDark(light8);
-                SwitchToDark(light9);SwitchToDark(light10);SwitchToDark(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToDark(light1);
+                SwitchToDark(light2);
+                SwitchToDark(light3);
+                SwitchToDark(light4);
+                SwitchToDark(light5);
+                SwitchToDark(light6);
+                SwitchToDark(light7);
+                SwitchToDark(light8);
+                SwitchToDark(light9);
+                SwitchToDark(light10);
+                SwitchToDark(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 1:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToDark(light3);SwitchToDark(light4);
-                SwitchToDark(light5);SwitchToDark(light6);SwitchToDark(light7);SwitchToDark(light8);
-                SwitchToDark(light9);SwitchToDark(light10);SwitchToDark(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToDark(light3);
+                SwitchToDark(light4);
+                SwitchToDark(light5);
+                SwitchToDark(light6);
+                SwitchToDark(light7);
+                SwitchToDark(light8);
+                SwitchToDark(light9);
+                SwitchToDark(light10);
+                SwitchToDark(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 2:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToLight(light3);SwitchToLight(light4);
-                SwitchToDark(light5);SwitchToDark(light6);SwitchToDark(light7);SwitchToDark(light8);
-                SwitchToDark(light9);SwitchToDark(light10);SwitchToDark(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToLight(light3);
+                SwitchToLight(light4);
+                SwitchToDark(light5);
+                SwitchToDark(light6);
+                SwitchToDark(light7);
+                SwitchToDark(light8);
+                SwitchToDark(light9);
+                SwitchToDark(light10);
+                SwitchToDark(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 3:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToLight(light3);SwitchToLight(light4);
-                SwitchToLight(light5);SwitchToLight(light6);SwitchToDark(light7);SwitchToDark(light8);
-                SwitchToDark(light9);SwitchToDark(light10);SwitchToDark(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToLight(light3);
+                SwitchToLight(light4);
+                SwitchToLight(light5);
+                SwitchToLight(light6);
+                SwitchToDark(light7);
+                SwitchToDark(light8);
+                SwitchToDark(light9);
+                SwitchToDark(light10);
+                SwitchToDark(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 4:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToLight(light3);SwitchToLight(light4);
-                SwitchToLight(light5);SwitchToLight(light6);SwitchToLight(light7);SwitchToLight(light8);
-                SwitchToLight(light9);SwitchToDark(light10);SwitchToDark(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToLight(light3);
+                SwitchToLight(light4);
+                SwitchToLight(light5);
+                SwitchToLight(light6);
+                SwitchToLight(light7);
+                SwitchToLight(light8);
+                SwitchToLight(light9);
+                SwitchToDark(light10);
+                SwitchToDark(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 5:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToLight(light3);SwitchToLight(light4);
-                SwitchToLight(light5);SwitchToLight(light6);SwitchToLight(light7);SwitchToLight(light8);
-                SwitchToLight(light9);SwitchToLight(light10);SwitchToLight(light11);SwitchToDark(light12);
-                SwitchToDark(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToLight(light3);
+                SwitchToLight(light4);
+                SwitchToLight(light5);
+                SwitchToLight(light6);
+                SwitchToLight(light7);
+                SwitchToLight(light8);
+                SwitchToLight(light9);
+                SwitchToLight(light10);
+                SwitchToLight(light11);
+                SwitchToDark(light12);
+                SwitchToDark(light13);
+                break;
             case 6:
-                SwitchToLight(light1);SwitchToLight(light2);SwitchToLight(light3);SwitchToLight(light4);
-                SwitchToLight(light5);SwitchToLight(light6);SwitchToLight(light7);SwitchToLight(light8);
-                SwitchToLight(light9);SwitchToLight(light10);SwitchToLight(light11);SwitchToLight(light12);
-                SwitchToLight(light13);break;
+                SwitchToLight(light1);
+                SwitchToLight(light2);
+                SwitchToLight(light3);
+                SwitchToLight(light4);
+                SwitchToLight(light5);
+                SwitchToLight(light6);
+                SwitchToLight(light7);
+                SwitchToLight(light8);
+                SwitchToLight(light9);
+                SwitchToLight(light10);
+                SwitchToLight(light11);
+                SwitchToLight(light12);
+                SwitchToLight(light13);
+                break;
         }
     }
 
-    private void SwitchToDark(ImageView light)
-    {
+    private void SwitchToDark(ImageView light) {
         light.setImageResource(R.mipmap.darker);
     }
 
-    private void SwitchToLight(ImageView light)
-    {
+    private void SwitchToLight(ImageView light) {
         light.setImageResource(R.mipmap.lighter);
     }
 }
--- app/src/main/java/com/lt/nexthud/testservice/MainActivity.java
+++ app/src/main/java/com/lt/nexthud/testservice/MainActivity.java
@@ -8,21 +8,15 @@ import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     private Intent intent;
-    private Button btn_start_service;
-    private Button btn_stop_service;
     private Button btn_add;
     private Button btn_minus;
-    private Button btn_unbundle;
     private int changeLight = 0;
-    private FxService msgService;
-    private boolean IS_START_SERVICE=false;
-    private boolean IS_START_BINDER=false;
-    private int defaultLightNumber=3;
+    private boolean IS_START_BINDER = false;
+    private int defaultLightNumber = 3;
+    private DelayDisappearWindowThread delayDisappearWindowThread = new DelayDisappearWindowThread();
 
     /* 通过Binder，实现Activity与Service通信 */
     private FxService.ServiceBinder mBinderService;
@@ -33,14 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            msgService = ((FxService.ServiceBinder)service).getService();
-
             mBinderService = (FxService.ServiceBinder) service;
-            try {
-                mBinderService.startDownload(changeLight);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            mBinderService.changeLight(changeLight);
         }
     };
 
@@ -48,105 +36,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_start_service=(Button)findViewById(R.id.button);
-        btn_stop_service=(Button)findViewById(R.id.button2);
-        btn_add=(Button)findViewById(R.id.button3);
-        btn_unbundle=(Button)findViewById(R.id.button4);
-        btn_minus=(Button)findViewById(R.id.button5);
-
-
-        btn_start_service.setOnClickListener(new android.view.View.OnClickListener() {
-            public void onClick(View v) {
-                intent = new Intent(MainActivity.this, FxService.class);
-                startService(intent);
-                IS_START_SERVICE=true;
-            }
-        });
-
-        btn_stop_service.setOnClickListener(new android.view.View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                intent = new Intent(MainActivity.this, FxService.class);
-                if(IS_START_SERVICE){
-                    stopService(intent);
-                }
-                IS_START_SERVICE=false;
-            }
-        });
+        btn_add = (Button) findViewById(R.id.button3);
+        btn_minus = (Button) findViewById(R.id.button5);
 
         btn_add.setOnClickListener(new android.view.View.OnClickListener() {
             public void onClick(View v) {
-                if(IS_START_BINDER)
-                {
-                    unbindService(connection);
-                }
-                if(defaultLightNumber + changeLight <6) {
+                if (defaultLightNumber + changeLight < 6) {
                     changeLight++;
                 }
-
-                intent = new Intent(MainActivity.this, FxService.class);
-                bindService(intent, connection, BIND_AUTO_CREATE);
-
-                IS_START_SERVICE=true;
-                IS_START_BINDER=true;
-                DelyUnbindService();
+                handleLight();
             }
         });
 
         btn_minus.setOnClickListener(new android.view.View.OnClickListener() {
             public void onClick(View v) {
-                if(IS_START_BINDER)
-                {
-                    unbindService(connection);
-                }
-                if(defaultLightNumber + changeLight >0) {
+                if (defaultLightNumber + changeLight > 0) {
                     changeLight--;
+                    handleLight();
                 }
-
-                intent = new Intent(MainActivity.this, FxService.class);
-                bindService(intent, connection, BIND_AUTO_CREATE);
-
-                IS_START_SERVICE=true;
-                IS_START_BINDER=true;
-                DelyUnbindService();
-            }
-        });
-
-
-        btn_unbundle.setOnClickListener(new android.view.View.OnClickListener() {
-            public void onClick(View v) {
-                if(IS_START_BINDER){
-                    unbindService(connection);
-                    Toast.makeText( MainActivity.this, "Binder unbind", Toast.LENGTH_SHORT).show();
-                }
-                IS_START_BINDER=false;
-                IS_START_SERVICE=false;
             }
         });
     }
 
-    private void DelyUnbindService()
-    {
-        new Thread() {
-            @Override
-            public void run() {
-                if (IS_START_BINDER) {
+    private void handleLight() {
+        if (IS_START_BINDER) {
+            mBinderService.changeLight(changeLight);
+        } else {
+            intent = new Intent(MainActivity.this, FxService.class);
+            bindService(intent, connection, BIND_AUTO_CREATE);
+        }
+        IS_START_BINDER = true;
+        handleDelayDisppearWindowThread();
+    }
+
+    private void handleDelayDisppearWindowThread() {
+        if (delayDisappearWindowThread.isAlive()) {
+            try {
+                delayDisappearWindowThread.interrupt();
+                delayDisappearWindowThread.join();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        delayDisappearWindowThread = new DelayDisappearWindowThread();
+        delayDisappearWindowThread.start();
+    }
+
+    public class DelayDisappearWindowThread extends Thread {
+        @Override
+        public void run() {
+            if (IS_START_BINDER) {
+                while (!isInterrupted()) {
                     try {
-                        sleep(3500);
-                        if(IS_START_BINDER)
-                        {
+                        sleep(4000);
+                        if (IS_START_BINDER) {
                             unbindService(connection);
-                            IS_START_BINDER=false;
-                            IS_START_SERVICE=false;
+                            IS_START_BINDER = false;
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
+                        break;
                     }
                 }
-            };
-        }.start();
+
+            }
+        }
     }
-
-
 }
